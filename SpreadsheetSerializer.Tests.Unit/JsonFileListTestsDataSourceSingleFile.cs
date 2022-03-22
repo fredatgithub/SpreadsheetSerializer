6b9832099184514fa2ab65b4a810f3a6a9b3cda3
fred <domaine75@gmail.com>
2022-03-22 14:09:53 +0100

    Remove license and clean code

--- SpreadsheetSerializer.Tests.Unit/JsonFileListTestsDataSourceSingleFile.cs
+++ SpreadsheetSerializer.Tests.Unit/JsonFileListTestsDataSourceSingleFile.cs
@@ -7,87 +7,87 @@ using System.IO;
 
 namespace SpreadsheetSerializer.Tests.Unit
 {
-    public class JsonFileListTestsDataSourceSingleFile
+  public class JsonFileListTestsDataSourceSingleFile
+  {
+    private const string DirectoryPath = @"..\..\..\DataSourceSingleFile\";
+
+    private static List<User> GetUsers()
     {
-        private const string DirectoryPath = @"..\..\..\DataSourceSingleFile\";
+      var users = new List<User>();
 
-        private static List<User> GetUsers()
-        {
-            var users = new List<User>();
+      var user1 = new User();
+      user1.Id = 1;
+      user1.Name = "Bob";
+      user1.EmailAddress = "bob@example.com";
+      users.Add(user1);
 
-            var user1 = new User();
-            user1.Id = 1;
-            user1.Name = "Bob";
-            user1.EmailAddress = "bob@example.com";
-            users.Add(user1);
-
-            var user2 = new User();
-            user2.Id = 2;
-            user2.Name = "Jill";
-            user2.EmailAddress = "jill@example.com";
-            users.Add(user2);
-            return users;
-        }
-
-        [Test]
-        public void CreateJsonFileFromList()
-        {
-            var users = GetUsers();
-
-            new JsonFileFromListSerializer<User>().Serialize(users, DirectoryPath);
-        }
-
-        [Test]
-        public void DeserializeFromJsonFileMatchesList()
-        {
-            var users = GetUsers();
-
-            new JsonFileFromListSerializer<User>().Serialize(users, DirectoryPath);
-            List<User> users2 = new JsonFileToListDeserializer<User>().Deserialize(DirectoryPath);
-
-            users.Should().BeEquivalentTo(users2);
-        }
-
-        /// <summary>
-        /// Outputs an empty JSON array in a User.json file in the current working directory of the running process and reads it in again
-        /// </summary>
-        [Test]
-        public void DeserializeEmptyList()
-        {
-            // ReSharper disable once CollectionNeverUpdated.Local
-            var users = new List<User>();
-            new JsonFileFromListSerializer<User>().Serialize(users);
-            List<User> users2 = new JsonFileToListDeserializer<User>().Deserialize();
-            users.Should().BeEquivalentTo(users2);
-        }
-
-        /// <summary>
-        /// Tests a fully qualified path.
-        /// Outputs an empty JSON array in an Empty.json file in the DataSourceSingleFile directory and reads it in again
-        /// </summary>
-        [Test]
-        public void DeserializeEmptyListWithNamedFile()
-        {
-            // ReSharper disable once CollectionNeverUpdated.Local
-            var users = new List<User>();
-            string path = Path.Combine(DirectoryPath, "Empty.json");
-            new JsonFileFromListSerializer<User>().Serialize(users, path);
-            List<User> users2 = new JsonFileToListDeserializer<User>().Deserialize(path);
-            users.Should().BeEquivalentTo(users2);
-        }
-
-        /// <summary>
-        /// Outputs an empty JSON array in an Empty2.json file in the current working directory of the running process and reads it in again
-        /// </summary>
-        [Test]
-        public void DeserializeEmptyListWithNamedFileToCurrentWorkingDirectory()
-        {
-            // ReSharper disable once CollectionNeverUpdated.Local
-            var users = new List<User>();
-            string path = "Empty2.json";
-            new JsonFileFromListSerializer<User>().Serialize(users, path);
-            List<User> users2 = new JsonFileToListDeserializer<User>().Deserialize(path);
-            users.Should().BeEquivalentTo(users2);
-        }
+      var user2 = new User();
+      user2.Id = 2;
+      user2.Name = "Jill";
+      user2.EmailAddress = "jill@example.com";
+      users.Add(user2);
+      return users;
     }
+
+    [Test]
+    public void CreateJsonFileFromList()
+    {
+      var users = GetUsers();
+
+      new JsonFileFromListSerializer<User>().Serialize(users, DirectoryPath);
+    }
+
+    [Test]
+    public void DeserializeFromJsonFileMatchesList()
+    {
+      var users = GetUsers();
+
+      new JsonFileFromListSerializer<User>().Serialize(users, DirectoryPath);
+      List<User> users2 = new JsonFileToListDeserializer<User>().Deserialize(DirectoryPath);
+
+      users.Should().BeEquivalentTo(users2);
+    }
+
+    /// <summary>
+    /// Outputs an empty JSON array in a User.json file in the current working directory of the running process and reads it in again
+    /// </summary>
+    [Test]
+    public void DeserializeEmptyList()
+    {
+      // ReSharper disable once CollectionNeverUpdated.Local
+      var users = new List<User>();
+      new JsonFileFromListSerializer<User>().Serialize(users);
+      List<User> users2 = new JsonFileToListDeserializer<User>().Deserialize();
+      users.Should().BeEquivalentTo(users2);
+    }
+
+    /// <summary>
+    /// Tests a fully qualified path.
+    /// Outputs an empty JSON array in an Empty.json file in the DataSourceSingleFile directory and reads it in again
+    /// </summary>
+    [Test]
+    public void DeserializeEmptyListWithNamedFile()
+    {
+      // ReSharper disable once CollectionNeverUpdated.Local
+      var users = new List<User>();
+      string path = Path.Combine(DirectoryPath, "Empty.json");
+      new JsonFileFromListSerializer<User>().Serialize(users, path);
+      List<User> users2 = new JsonFileToListDeserializer<User>().Deserialize(path);
+      users.Should().BeEquivalentTo(users2);
+    }
+
+    /// <summary>
+    /// Outputs an empty JSON array in an Empty2.json file in the current working directory of the running process and reads it in again
+    /// </summary>
+    [Test]
+    public void DeserializeEmptyListWithNamedFileToCurrentWorkingDirectory()
+    {
+      // ReSharper disable once CollectionNeverUpdated.Local
+      var users = new List<User>();
+      string path = "Empty2.json";
+      new JsonFileFromListSerializer<User>().Serialize(users, path);
+      List<User> users2 = new JsonFileToListDeserializer<User>().Deserialize(path);
+      users.Should().BeEquivalentTo(users2);
+    }
+  }
 }
--- SpreadsheetSerializer.Tests.Unit/SerializerTestsDataSource1.cs
+++ SpreadsheetSerializer.Tests.Unit/SerializerTestsDataSource1.cs
@@ -7,85 +7,86 @@ using System.IO;
 
 namespace SpreadsheetSerializer.Tests.Unit
 {
-    public class SerializerTestsDataSource1
+  public class SerializerTestsDataSource1
+  {
+    // JsonFileFromListSerializer requires the \ on the end of the directory path or it will create a file named DataSource1.json
+    // none of the other tools require the \ at the end
+    private const string DirectoryPath = @"..\..\..\DataSource1\";
+    private ExampleWorkbook workbook;
+
+    /// <summary>
+    /// Setup tests by setting the Aspose license and reading the DataSource1 JSON directory into the workbook object.
+    /// Uncomment the Excel Editing section of this method to update test cases from saved changes in Excel.
+    /// </summary>
+    [SetUp]
+    public void Setup()
     {
-        // JsonFileFromListSerializer requires the \ on the end of the directory path or it will create a file named DataSource1.json
-        // none of the other tools require the \ at the end
-        private const string DirectoryPath = @"..\..\..\DataSource1\";
-        private ExampleWorkbook workbook;
+      // The Aspose license file should be dropped into the build folder (Ex: bin/Debug/net5.0/). It should never be checked into git.
+      //License.SetLicense("Aspose.Total.lic");
+      // do it with a trial version no need license to test
 
-        /// <summary>
-        /// Setup tests by setting the Aspose license and reading the DataSource1 JSON directory into the workbook object.
-        /// Uncomment the Excel Editing section of this method to update test cases from saved changes in Excel.
-        /// </summary>
-        [SetUp]
-        public void Setup()
-        {
-            // The Aspose license file should be dropped into the build folder (Ex: bin/Debug/net5.0/). It should never be checked into git.
-            SpreadsheetSerializer.AsposeCells.License.SetLicense("Aspose.Total.lic");
-
-            // Uncomment this section when editing test cases in Excel.
-            // Otherwise comment out this section because the single source of truth is JSON, which can be checked into git and diffed
-            // Excel Editing:
-            //var wbDeserializer = new WorkbookDeserializer<ExampleWorkbook>();
-            //var workbookExample = wbDeserializer.Deserialize(Path.Combine(DirectoryPath, nameof(ExampleWorkbook)));
-            //new JsonDirectoryFromClassSerializer().Serialize(workbookExample, DirectoryPath);
+      // Uncomment this section when editing test cases in Excel.
+      // Otherwise comment out this section because the single source of truth is JSON, which can be checked into git and diffed
+      // Excel Editing:
+      //var wbDeserializer = new WorkbookDeserializer<ExampleWorkbook>();
+      //var workbookExample = wbDeserializer.Deserialize(Path.Combine(DirectoryPath, nameof(ExampleWorkbook)));
+      //new JsonDirectoryFromClassSerializer().Serialize(workbookExample, DirectoryPath);
 
 
-            // Method 1: Json directory to class object instance
-            string dataSource1ClassPath = Path.Combine(DirectoryPath, nameof(ExampleWorkbook));
-            workbook = new JsonDirectoryToClassDeserializer<ExampleWorkbook>().Deserialize(dataSource1ClassPath);
+      // Method 1: Json directory to class object instance
+      string dataSource1ClassPath = Path.Combine(DirectoryPath, nameof(ExampleWorkbook));
+      workbook = new JsonDirectoryToClassDeserializer<ExampleWorkbook>().Deserialize(dataSource1ClassPath);
 
-            // Method 2: Json directory to json string
-            //string json = new JsonDirectoryToStringDeserializer().Deserialize(dataSource1ClassPath);
-            //workbook = JsonConvert.DeserializeObject<ExampleWorkbook>(json);
-        }
-
-
-        /// <summary>
-        /// Uncomment this Test to create an Excel workbook from the JSON directory so that the test cases can be edited in Excel.
-        /// Comment it out again after Excel workbook has been made.
-        /// The Excel workbook does not need to be checked into git
-        /// </summary>
-        [Test]
-        public void CreateExcelWorkbook()
-        {
-            //string dataSource1ClassPath = Path.Combine(DirectoryPath, nameof(ExampleWorkbook));
-            //var workbookExample = new JsonDirectoryToClassDeserializer<ExampleWorkbook>().Deserialize(dataSource1ClassPath);
-
-            //var myWorkbookSerializer = new WorkbookSerializer<ExampleWorkbook>();
-            //myWorkbookSerializer.Serialize(workbookExample, dataSource1ClassPath);
-        }
-
-        /// <summary>
-        /// Uncomment this Test to create a JSON directory from an Excel workbook so that the test cases can be edited in JSON.
-        /// Comment it out again after the JSON directory has been made because the SetUp method will read from JSON by default.
-        /// The JSON directory test cases should be checked into git, but the Excel workbook does not need to be checked in.
-        /// Other developers can re-create the Excel workbook by uncommenting the CreateExcelWorkbook() Test
-        /// </summary>
-        [Test]
-        public void CreateJsonDirectory()
-        {
-            //var wbDeserializer = new WorkbookDeserializer<ExampleWorkbook>();
-            //var workbookExample = wbDeserializer.Deserialize(Path.Combine(DirectoryPath, nameof(ExampleWorkbook)));
-            //new JsonDirectoryFromClassSerializer().Serialize(workbookExample, DirectoryPath);
-        }
-
-        /// <summary>
-        /// This test can be used to verify that the Excel workbook matches the JSON directory
-        /// (after the Excel workbook has been created by executing the CreateExcelWorkbook() Test)
-        /// </summary>
-        [Test]
-        public void DeserializeFromExcelMatchesDeserializeFromTextFiles()
-        {
-            // Uncomment this section or run the CreateExcelWorkbook() Test to create the Excel workbook from JSON directory first
-            //var myWorkbookSerializer = new WorkbookSerializer<ExampleWorkbook>();
-            //myWorkbookSerializer.Serialize(workbook, DirectoryPath);
-
-            var wbDeserializer = new WorkbookDeserializer<ExampleWorkbook>();
-            var myWorkbookExample = wbDeserializer.Deserialize(Path.Combine(DirectoryPath, nameof(ExampleWorkbook)));
-
-            myWorkbookExample.Should().BeEquivalentTo(workbook);
-        }
+      // Method 2: Json directory to json string
+      //string json = new JsonDirectoryToStringDeserializer().Deserialize(dataSource1ClassPath);
+      //workbook = JsonConvert.DeserializeObject<ExampleWorkbook>(json);
     }
+
+
+    /// <summary>
+    /// Uncomment this Test to create an Excel workbook from the JSON directory so that the test cases can be edited in Excel.
+    /// Comment it out again after Excel workbook has been made.
+    /// The Excel workbook does not need to be checked into git
+    /// </summary>
+    [Test]
+    public void CreateExcelWorkbook()
+    {
+      string dataSource1ClassPath = Path.Combine(DirectoryPath, nameof(ExampleWorkbook));
+      var workbookExample = new JsonDirectoryToClassDeserializer<ExampleWorkbook>().Deserialize(dataSource1ClassPath);
+
+      var myWorkbookSerializer = new WorkbookSerializer<ExampleWorkbook>();
+      myWorkbookSerializer.Serialize(workbookExample, dataSource1ClassPath);
+    }
+
+    /// <summary>
+    /// Uncomment this Test to create a JSON directory from an Excel workbook so that the test cases can be edited in JSON.
+    /// Comment it out again after the JSON directory has been made because the SetUp method will read from JSON by default.
+    /// The JSON directory test cases should be checked into git, but the Excel workbook does not need to be checked in.
+    /// Other developers can re-create the Excel workbook by uncommenting the CreateExcelWorkbook() Test
+    /// </summary>
+    [Test]
+    public void CreateJsonDirectory()
+    {
+      //var wbDeserializer = new WorkbookDeserializer<ExampleWorkbook>();
+      //var workbookExample = wbDeserializer.Deserialize(Path.Combine(DirectoryPath, nameof(ExampleWorkbook)));
+      //new JsonDirectoryFromClassSerializer().Serialize(workbookExample, DirectoryPath);
+    }
+
+    /// <summary>
+    /// This test can be used to verify that the Excel workbook matches the JSON directory
+    /// (after the Excel workbook has been created by executing the CreateExcelWorkbook() Test)
+    /// </summary>
+    [Test]
+    public void DeserializeFromExcelMatchesDeserializeFromTextFiles()
+    {
+      // Uncomment this section or run the CreateExcelWorkbook() Test to create the Excel workbook from JSON directory first
+      //var myWorkbookSerializer = new WorkbookSerializer<ExampleWorkbook>();
+      //myWorkbookSerializer.Serialize(workbook, DirectoryPath);
+
+      var wbDeserializer = new WorkbookDeserializer<ExampleWorkbook>();
+      var myWorkbookExample = wbDeserializer.Deserialize(Path.Combine(DirectoryPath, nameof(ExampleWorkbook)));
+
+      myWorkbookExample.Should().BeEquivalentTo(workbook);
+    }
+  }
 }
